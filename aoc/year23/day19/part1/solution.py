e6a9c1a102c214b0a019858325dfa2ca7eb4f1a5
Florian Daude <floriandaude@hotmail.fr>
2024-11-02 15:18:45 +0100

    added day19 part2

--- aoc/year23/day19/part1/solution.py
+++ aoc/year23/day19/part1/solution.py
@@ -1,5 +1,7 @@
 import dataclasses
 import enum
+import typing
+
 from aoc import helpers
 
 
@@ -10,6 +12,22 @@ class Operation(enum.Enum):
 
 type Result = str | bool
 type Object = dict[str, int]
+type ObjectRange = dict[str, range]
+
+
+def object_range_intersect(or1: ObjectRange, or2: ObjectRange):
+    result: ObjectRange = {}
+    for k in or1 | or2:
+        r1 = or1.get(k)
+        r2 = or2.get(k)
+        u = r1 or r2
+        if u is None:
+            continue
+        if r1 is None or r2 is None:
+            result[k] = u
+        else:
+            result[k] = range(max(r1.start, r2.start), min(r1.stop, r2.stop))
+    return result
 
 
 def parse_result(s: str):
@@ -20,7 +38,7 @@ def parse_result(s: str):
     return s
 
 
-@dataclasses.dataclass
+@dataclasses.dataclass(frozen=True)
 class Condition:
     op: Operation
     lhs: str
@@ -45,8 +63,24 @@ class Condition:
             case Operation.GT:
                 return lhs > self.rhs
 
+    def get_range(self, *, inverted: bool = False):
+        match self.op:
+            case Operation.LT:
+                if not inverted:
+                    return {self.lhs: range(1, self.rhs)}
+                else:
+                    return {self.lhs: range(self.rhs, 4001)}
+            case Operation.GT:
+                if not inverted:
+                    return {self.lhs: range(self.rhs + 1, 4001)}
+                else:
+                    return {self.lhs: range(1, self.rhs + 1)}
 
-@dataclasses.dataclass
+
+WorkflowRangesGetter: typing.TypeAlias = typing.Callable[[str], list[ObjectRange]]
+
+
+@dataclasses.dataclass(frozen=True)
 class Step:
     condition: Condition | None
     result: Result
@@ -62,8 +96,34 @@ class Step:
         if self.condition is None or self.condition.apply(object):
             return self.result
 
+    def get_ranges(
+        self, workflow_ranges_getter: WorkflowRangesGetter, previous_conditions: list[Condition]
+    ) -> list[ObjectRange]:
+        base_obj_ranges: list[ObjectRange]
+        match self.result:
+            case True:
+                base_obj_ranges = [{}]
+            case False:
+                base_obj_ranges = []
+            case str():
+                base_obj_ranges = workflow_ranges_getter(self.result)
 
-@dataclasses.dataclass
+        condition_obj_ranges = [
+            condition.get_range(inverted=True) for condition in previous_conditions
+        ] + ([self.condition.get_range(inverted=False)] if self.condition is not None else [])
+
+        obj_ranges: list[ObjectRange] = []
+        for obj_range in base_obj_ranges:
+            for condition_obj_range in condition_obj_ranges:
+                obj_range = object_range_intersect(obj_range, condition_obj_range)
+            if any(len(range) == 0 for range in obj_range.values()):
+                continue
+            obj_ranges.append(obj_range)
+
+        return obj_ranges
+
+
+@dataclasses.dataclass(frozen=True)
 class Workflow:
     name: str
     steps: list[Step]
@@ -80,6 +140,15 @@ class Workflow:
             if (result := step.apply(object)) is not None:
                 return result
         raise RuntimeError('No result')
+
+    def get_ranges(self, workflow_ranges_getter: WorkflowRangesGetter):
+        obj_ranges: list[ObjectRange] = []
+        previous_conditions: list[Condition] = []
+        for step in self.steps:
+            obj_ranges.extend(step.get_ranges(workflow_ranges_getter, previous_conditions))
+            if step.condition is not None:
+                previous_conditions.append(step.condition)
+        return obj_ranges
 
 
 def parse_object(s: str):
@@ -124,6 +193,18 @@ class Input:
                     return result
                 case str():
                     current_workflow = self.workflow_map[result]
+
+    def get_ranges(self):
+        workflow_obj_ranges: dict[str, list[ObjectRange]] = {}
+
+        def workflow_ranges_getter(name: str) -> list[ObjectRange]:
+            obj_ranges = workflow_obj_ranges.get(name)
+            if obj_ranges is None:
+                obj_ranges = self.workflow_map[name].get_ranges(workflow_ranges_getter)
+                workflow_obj_ranges[name] = obj_ranges
+            return obj_ranges
+
+        return workflow_ranges_getter('in')
 
     def apply_all(self):
         for o in self.objects:
--- aoc/year23/day19/part2/solution.py
+++ aoc/year23/day19/part2/solution.py
@@ -1,9 +1,21 @@
+import functools
+import typing
+
 from aoc import helpers
-from aoc.year23.day19.part1 import solution as p1  # pyright: ignore[reportUnusedImport]
+from aoc.year23.day19.part1 import solution as p1
+
+ObjectRange: typing.TypeAlias = dict[str, range]
+
+
+def obj_range_size(obj_range: p1.ObjectRange):
+    return functools.reduce(
+        lambda a, b: a * b, [len(obj_range.get(k, range(1, 4001))) for k in 'xmas']
+    )
 
 
 def solution(lines: list[str]):
-    return 0
+    input = p1.Input.parse(lines)
+    return sum(obj_range_size(obj_range) for obj_range in input.get_ranges())
 
 
 if __name__ == '__main__':
